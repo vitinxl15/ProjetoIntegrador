6e9bc6e143f2f08b51f5124c76e52b73cb195194
Rafael3323 <rafael.albuquerque1@outlook.com>
2025-09-04 21:25:28 -0300

    fix
    correção de responsividade da tela de perfil

---
+++ script/perfil.js
@@ -0,0 +1,49 @@
+<<<<<<< Updated upstream:script/pernil.js
+const pernilCategory = document.querySelectorAll(".pernilCategory");
+document.querySelector(".pernilCategory").addEventListener("click", () => {
+    /* adiciona um Li com o texto: "irineu" ao ul */
+    const li = document.createElement("li");
+    li.textContent = "irineu";
+    const ul = document.querySelector(".pernilLista");
+    ul.appendChild(li);
+
+
+=======
+const perfilCategory = document.querySelectorAll(".perfilCategory");
+const lista=document.querySelector("#perfilList");
+const dog = document.querySelector("#infoDog");
+const dogList = document.querySelector("#cachorros");
+const perfil = document.querySelector("#infoPerfil");
+const agendamentos = document.querySelector("#agendamentos");
+const info = document.querySelector("#informacoes");
+const agenda = document.querySelector("#agenda");
+
+/* adiciona um Li com o texto: "irineu" na lista ao clicar no botão(perfilCategory) */
+
+perfilCategory.forEach((category) => {
+    category.addEventListener("click", () => {
+        const li = document.createElement("li");
+        li.textContent = "irineu";
+        lista.appendChild(li);
+    });
+});
+/*ao clicar na variável dog, ou agendamentos, oculta o campo informacoes */
+
+dog.addEventListener("click", () => {
+    info.style.display = "none";
+    dogList.style.display = "flex";
+    agenda.style.display = "none";
+});
+agendamentos.addEventListener("click", () => {
+    info.style.display = "none";
+    dogList.style.display = "none";
+    agenda.style.display = "block";
+});
+perfil.addEventListener("click", () => {
+    info.style.display = "flex";
+    dogList.style.display = "none";
+    agenda.style.display = "none";
+});
+>>>>>>> Stashed changes:script/perfil.js
+
+});
