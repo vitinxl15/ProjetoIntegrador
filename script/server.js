2be05e674d87509c211d16a6d8ba3e77b686cfa9
João Vitor <hadezxl556@gmail.com>
2026-01-26 20:44:31 -0300

    update
    mexendo no styles das paginas

--- script/server.js
+++ script/server.js
@@ -1,7 +1,7 @@
 // server.js
 
 const supabaseUrl = "https://uhhagvmmxtcavngjdaik.supabase.co"
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVoaGFndm1teHRjYXZuZ2pkYWlrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTgyMzQ5MTQsImV4cCI6MjA3MzgxMDkxNH0.myBAOKrgVRKi82SeGC9r_P1N1-Z9tLtvN2cpk_MCYdQ"  // ⚠️ substitua pela sua anon key
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVoaGFndm1teHRjYXZuZ2pkYWlrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTgyMzQ5MTQsImV4cCI6MjA3MzgxMDkxNH0.myBAOKrgVRKi82SeGC9r_P1N1-Z9tLtvN2cpk_MCYdQ"  
 const supabaseClient = supabase.createClient(supabaseUrl, supabaseKey)
 
 // intercepta o form
